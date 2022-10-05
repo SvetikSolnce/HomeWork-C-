d7dd89a942eda6af3a65abfe1455115f55335f5c
Mr S <ilgizs@list.ru>
2022-10-05 13:39:21 +0300

    решили вторую  домашнюю задачу

---
+++ Lesson01-task04/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите три числa");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c; 
+Console.Write("max = ");
+Console.WriteLine(max);
