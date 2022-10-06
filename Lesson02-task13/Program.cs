e3d74b577c44d6756cf1639002f92e54fd38a44e
Mr S <ilgizs@list.ru>
2022-10-07 00:47:10 +0300

    решили 2  задачу

--- Lesson02-task13/Program.cs
+++ Lesson02-task13/Program.cs
@@ -1,7 +1,13 @@
-﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-Console.Write("Введите цифру от 1 до 7:");
-int num = int.Parse(Console.ReadLine());
-if (num == 6) Console.WriteLine("ДА");
-else if (num == 7) Console.WriteLine("ДА");
-else Console.WriteLine("НЕТ");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число:");
+string? str = Console.ReadLine();
+char[] array = new char[str.Length];
+if (str.Length < 3)  Console.WriteLine("Третьей цифры нет");
+else {
+    for (int i = 0; i < str.Length; i++) {
+    array[i] = str[i];  
+    }
+Console.WriteLine(array[2]);
+}
+
 
