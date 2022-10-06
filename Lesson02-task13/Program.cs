9ce0eb39e8285c2a802a46c56a8365f9d6ddc3ce
Mr S <ilgizs@list.ru>
2022-10-06 22:25:53 +0300

    решили 3 задачу

--- Lesson02-task13/Program.cs
+++ Lesson02-task13/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите цифру от 1 до 7:");
+int num = int.Parse(Console.ReadLine());
+if (num == 6) Console.WriteLine("ДА");
+else if (num == 7) Console.WriteLine("ДА");
+else Console.WriteLine("НЕТ");
+
