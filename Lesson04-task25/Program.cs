53f15531737148007910f43f392776c9e3fdb790
Mr S <ilgizs@list.ru>
2022-10-12 23:46:04 +0300

    решили 1 задачу 4 семинара

---
+++ Lesson04-task25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Сделать в функции, сделать проверку на отрицательность.
+
+void fun (int A, int B) {
+    double stepen = 1;
+    if (A>0 && B>0) {
+        stepen = Math.Pow(A, B);
+        Console.WriteLine($"Степень числа: {stepen}");
+    }
+    //else Console.WriteLine("Введите положительные числа");
+}
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+fun(A, B);
+
+/*int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+double stepen = Math.Pow(A, B);
+Console.WriteLine(stepen);*/
