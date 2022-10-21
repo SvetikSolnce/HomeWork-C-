62fde6cb33ad2a9c3cf42a6e3b1473cbbcb820f7
Mr S <ilgizs@list.ru>
2022-10-21 17:33:50 +0300

    решили 1 задачу 6 семинара

---
+++ Lesson06-task41/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите числа через запятую:");
+int[] m = Console.ReadLine().Split(',').Select(e => Convert.ToInt32(e)).ToArray();
+void Summa (int[] m) {
+    int count = 0;
+    for (int i=0; i<m.Length; i++) {
+        if (m[i]>0) count = count + 1;
+    }
+    Console.WriteLine(count);    
+}
+void Print(int [] m) {
+    Console.WriteLine(String.Join(" ", m));
+}
+Print(m);
+Summa(m);
+
+
+
