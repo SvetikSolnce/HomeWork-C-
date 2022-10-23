97d9733ed987a264b7872b66a78d5261eaaf9427
Mr S <ilgizs@list.ru>
2022-10-23 14:38:18 +0300

    решили 1 задачу 7 семинара

---
+++ Lesson07-task47/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+void PrintArray(int[,] matr) {
+    for (int i = 0; i<matr.GetLength(0); i++) {
+    for (int j=0; j<matr.GetLength(1); j++) {
+        Console.Write($"{matr[i, j]} ");
+    }
+    Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr) {
+    for (int i = 0; i<matr.GetLength(0); i++) {
+    for (int j=0; j<matr.GetLength(1); j++) {
+        matr[i,j] = new Random().Next(1, 10);
+    }
+    }
+}
+Console.Write("Введите n:");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите m:");
+int m = int.Parse(Console.ReadLine());
+int[,] matrix = new int [n, m];
+FillArray(matrix);
+PrintArray(matrix);
