0d65b2236ba80bee48ad622a48b1613cf3ebba61
Mr S <ilgizs@list.ru>
2022-11-11 22:53:42 +0300

    решила 1 задачу 8 семинара

---
+++ Lesson08-task54/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void FillArray(int[,] arr) {
+    for (int i = 0; i<arr.GetLength(0); i++) {
+    for (int j=0; j<arr.GetLength(1); j++) {
+       arr[i,j] = new Random().Next(1, 10);
+    }
+    }
+}
+
+void PrintArray(int[,] arr) {
+    for (int i = 0; i<arr.GetLength(0); i++) {
+    for (int j=0; j<arr.GetLength(1); j++) {
+        Console.Write($"{arr[i, j]} ");
+    }
+    Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] arr) {
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      for (int k = 0; k < arr.GetLength(1) - 1; k++)
+      {
+        if (arr[i, k] < arr[i, k + 1])
+        {
+          int temp = arr[i, k + 1];
+          arr[i, k + 1] = arr[i, k];
+          arr[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+int[,] array = new int [5, 5];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+SortArray(array);
+PrintArray(array);
