6d32a47bbbf34925ce214bd6ef7dd1af212eb80c
Mr S <ilgizs@list.ru>
2022-11-14 09:33:54 +0300

    решила 1  задачу 9 семинара

---
+++ Lesson09-task64/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+string FillNumbers (int n) {
+    if (n==1) return "1";
+    else
+        return (n + " " + FillNumbers(n-1));
+}
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"N = {num} -> {FillNumbers(num)}");
